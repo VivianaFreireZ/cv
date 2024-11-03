68fd0ac7476bab1a17d44993d78035a31b35d3b9
VivianaFreireZ <viviana.freire91@outlook.com>
2024-11-03 16:12:52 -0700

    adding data

---
+++ initialize_cv.R
@@ -0,0 +1,7 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Christian Ayala-Ortiz",
+  data_location = "https://docs.google.com/spreadsheets/d/1g9hSYRN3d5YCYvYRQpsHMfFym8XQmogE7N7lduxNja0",
+  pdf_location = "https://github.com/Coayala/cv/raw/main/pdf/coayala_cv.pdf",
+  html_location = "https://github.com/Coayala/cv/raw/main/docs/",
+  source_location = "https://github.com/Coayala/cv"
+)
